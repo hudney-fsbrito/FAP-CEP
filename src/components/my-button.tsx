0348e3a3a605a6514b9bc494961b04895ef99100
hudney-fsbrito <hudney.rse@gmail.com>
2024-10-30 13:42:56 -0300

    feat: add em uma array os dados de endereço

--- src/components/my-button.tsx
+++ src/components/my-button.tsx
@@ -8,56 +8,78 @@ export function MyButton() {
   const [neighborhood, setNeighborhood] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const [textValue,setTextVelue] = useState('')
+  const [textValue, setTextVelue] = useState("");
+  const [addresses, setAddresses] = useState([]);
 
   async function handleGetAddress() {
-    setLoading(true)
-    try{
-
+    setLoading(true);
+    try {
       const result = await getAddress(textValue);
       console.log(result);
       console.log(textValue);
-      
+
       setAdress(result.logradouro);
       setCep(result.cep);
       setNeighborhood(result.bairro);
-    } catch (error){
+
+      setAddresses((arrAddress) => [
+        {
+          logradouro: result.logradouro,
+          cep: result.cep,
+          bairro: result.bairro,
+        },
+        ...arrAddress,
+
+      ]);
+    } catch (error) {
       console.log(error);
-      alert("Ocorrey um erro ao obter o endereço.")
-    } finally{
-      setLoading(false)
+      alert("Ocorrey um erro ao obter o endereço.");
+    } finally {
+      setLoading(false);
     }
-    
   }
 
   return (
-    <div className="flex flex-row gap-5">
-      
-      <div className="flex flex-col">
-      <label htmlFor="">CEP</label>
-      <input 
-        onChange={(e)=>{setTextVelue(e.target.value)}}
-        className="border border-s-4 border-b-primary p-5 rounded-2xl" 
-        type="text" 
-        placeholder="Digite um CEP válido"
-      />
+    <>
+      <div className="flex flex-row gap-5">
+        <div className="flex flex-col">
+          <label htmlFor="">CEP</label>
+          <input
+            onChange={(e) => {
+              setTextVelue(e.target.value);
+            }}
+            className="border border-s-4 border-b-primary p-5 rounded-2xl"
+            type="text"
+            placeholder="Digite um CEP válido"
+          />
+        </div>
+
+        <button
+          disabled={textValue == "" ? true : false}
+          onClick={handleGetAddress}
+          className="border border-blue-950 px-8 py-3 bg-primary text-[#fff] rounded-3xl"
+        >
+          Button
+        </button>
+
+        <div>
+          {/* {String(loading)} */}
+          {loading ? "Carregando..." : "Obter endereço"}
+
+          <div>Endereço: {adress}</div>
+          <div>cep: {cep}</div>
+          <div>Bairro: {neighborhood}</div>
+        </div>
       </div>
-
-      <button
-        disabled={textValue == "" ? true : false}
-        onClick={handleGetAddress}
-        className="border border-blue-950 px-8 py-3 bg-primary text-[#fff] rounded-3xl"
-      >
-        Button
-      </button>
-
       <div>
-        {/* {String(loading)} */}
-        {loading? "Carregando..." : "Obter endereço"}
-        <div>Endereço: {adress}</div>
-        <div>cep: {cep}</div>
-        <div>Bairro: {neighborhood}</div>
+        {addresses.map((address, index) => (
+          <div key={index} className="mt-4">
+            <div>Endereço: {address.logradouro}</div>
+            <div>CEP: {address.cep}</div>
+            <div>Bairro: {address.bairro}</div>
+          </div>
+        ))}
       </div>
-    </div>
+    </>
   );
 }
