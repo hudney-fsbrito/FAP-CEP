5ce0e3f43c4dda0b2750fcab22e2c62a531dd46c
hudney-fsbrito <hudney.rse@gmail.com>
2024-11-12 18:37:56 -0300

    feat: add imagem de fundo e animações

--- src/app/components/my-button.tsx
+++ src/app/components/my-button.tsx
@@ -9,14 +9,14 @@ export function MyButton({ onFetchAddress }: MyButtonProps) {
   const [textValue, setTextValue] = useState("");
 
   return (
-    <div className="flex flex-row gap-5">
+    <div className="flex flex-col gap-3">
       <div className="flex flex-col">
         <label htmlFor="cep-input">CEP</label>
         <input
           id="cep-input"
           value={textValue}
           onChange={(e) => setTextValue(e.target.value)}
-          className="border border-s-4 border-b-primary p-5 rounded-2xl"
+          className="border border-primary p-2 rounded-md shadow-[40px] focus:outline-0"
           type="text"
           placeholder="Digite um CEP válido"
         />
@@ -24,7 +24,7 @@ export function MyButton({ onFetchAddress }: MyButtonProps) {
       <button
         disabled={!textValue}
         onClick={() => onFetchAddress(textValue)}
-        className="border border-blue-950 px-8 py-3 bg-primary text-[#fff] rounded-3xl"
+        className="w-full border border-blue-950 p-2 bg-primary text-[#fff] rounded-2xl"
       >
         Buscar Endereço
       </button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { MyButton } from "./components/my-button";
 import { MdOutlineDelete } from "react-icons/md";
 import { fetchAddress, formatDate } from "./addressUtil";
+import { motion } from "framer-motion";
+// import imgBackground from "../../public/backgroundMapa.jpg"
 
 export type Address = {
   id: string;
@@ -18,7 +20,7 @@ export type Address = {
 
 export const initialAddress: Address[] = [
   {
-    id: self.crypto.randomUUID(),
+    id: crypto.randomUUID(),
     bairro: "Bairro",
     estado: "Estado",
     localidade: "Cidade",
@@ -43,20 +45,70 @@ export default function Home() {
   };
 
   const handleDeleteAddress = (id: string) => {
-    setAddresses(addresses.filter((address) => address.id !== id));
+    console.log(id);
+    const filteredAddresses = addresses.filter((address) => address.id !== id);
+    console.log(filteredAddresses);
+    setAddresses(filteredAddresses);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-1 pb-20 gap-12 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="flex justify-center text-4xl">CEP Torpedo</h1>
-      <MyButton onFetchAddress={handleFetchAddress} />
+    <div className="relative flex flex-col items-center justify-center min-h-screen p-1 pb-20 gap-12 font-[family-name:var(--font-geist-sans)]">
+      <motion.img
+        src="/backgroundMapa.jpg"
+        alt={"Imagem de um mapa como imagem de fundo"}
+        className="absolute top-0 bottom-[-10px] bg-gradient-to-tl brightness-75 w-full h-full object-cover 
+      opacity-50 z-0"
+        initial={{ scale: 2.5 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 0.3 }}
+      />
+
+      <motion.h1
+        className="flex justify-center text-4xl font-semibold z-10"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.8 }}
+      >
+        CEP Torpedo
+        <motion.span
+          // initial={{ opacity: 0, y: 20 }}
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 180, 180, 0],
+            borderRadius: ["0%", "0%", "50%", "50%", "0%"],
+          }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+            // repeat: Infinity,
+            repeatDelay: 1,
+          }}
+        >
+          🚀
+        </motion.span>
+      </motion.h1>
+
+      <motion.div
+        className="w-96 p-10 bg-primary bg-opacity-15 rounded-md flex flex-col gap-5 z-10"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.8 }}
+      >
+        <MyButton onFetchAddress={handleFetchAddress} />
+      </motion.div>
 
       {loading && <p>Carregando...</p>}
 
       {addresses.length > 0 && (
-        <table className="table-auto [&>*>*>*]:border-2">
-          <thead>
-            <tr className="[&>*]:px-4 [&>*]:py-2">
+        <motion.table
+          className="table-auto bg-primary bg-opacity-15 rounded-md [&>*>*>*]:border-2 z-10 "
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.8 }}
+        >
+          <thead className="">
+            <tr className=" [&>*]:px-4 [&>*]:py-2">
               <th>Logradouro</th>
               <th>Bairro</th>
               <th>Localidade</th>
@@ -86,7 +138,7 @@ export default function Home() {
               </tr>
             ))}
           </tbody>
-        </table>
+        </motion.table>
       )}
     </div>
   );
